3c27016ee508d5a674bea744e574bd070af0f628
NutanpramodDeshmukh <nutanpd17@gmail.com>
2023-02-19 23:27:03 +0530

    Added: Initial message

---
+++ ConsoleApp1/MoodAnalyaser.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class MoodAnalyaser
+    {
+        private string message;
+
+        public MoodAnalyaser(string message)
+        {
+            this.message = message;
+        }
+
+        public string AnalyseMood()
+        {
+            try
+            {
+                if (this.message.Equals(string.Empty))
+                {
+                    throw new CustomException(CustomException.ExceptionType.Empty_Message, "Empty Mood message not valid");
+
+                }
+                else if (this.message.Contains("Sad"))
+                {
+                    return "SAD";
+                }
+                else
+                {
+                    return "HAPPY";
+                }
+            }
+            catch (NullReferenceException)
+            {
+                throw new CustomException(CustomException.ExceptionType.Null_Message, "Null message not valid");
+
+            }
+        }
+
+    }
+}
---
+++ UnitTestProject2/UnitTest1.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using NUnit.Framework;
+using MoodAnalyaser;
+
+namespace UnitTestProject2
+{
+    
+    public class Tests
+    {
+
+        [SetUp]
+
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+
+        public void EmptyMessageShouldThrowCustomException_EmptyMeaasage()
+        {
+            string expecxted = "Empty Mood message not valid";
+            try
+            {
+                MoodAnalyaser analyaser = new MoodAnalyaser("");
+                string mood = analyaser.AnalyseMood();
+            }
+            catch (CustomException e)
+            {
+                Assert.AreEqual(expecxted, e.Message);
+            }
+
+        }
+
+        [Test]
+
+        public void NullMessageShouldThrowCustomException_NullMeaasage()
+        {
+            string expecxted = "Null message not valid";
+            try
+            {
+                MoodAnalyaser analyaser = new MoodAnalyaser(null);
+                string mood = analyaser.AnalyseMood();
+            }
+            catch (CustomException e)
+            {
+                Assert.AreEqual(expecxted, e.Message);
+            }
+        }
+
+    }
+}
